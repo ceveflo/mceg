77e06702dff00abf321a1971cb55d37000a4cdae
e <e>
2014-09-29 11:21:41 -0600

    assets issue 1

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,29 +1,29 @@
-# AssetSync.configure do |config|
-#   config.fog_provider = 'Rackspace'
-#   config.rackspace_username = 'oscar.navidad'
-#   config.rackspace_api_key = '6fbc0378417246b3bbe3b5b60ff40381'
+AssetSync.configure do |config|
+  config.fog_provider = 'Rackspace'
+  config.rackspace_username = 'oscar.navidad'
+  config.rackspace_api_key = '6fbc0378417246b3bbe3b5b60ff40381'
 
-#   # if you need to change rackspace_auth_url (e.g. if you need to use Rackspace London)
-#   # config.rackspace_auth_url = "lon.auth.api.rackspacecloud.com"
-#   config.fog_directory = 'mediacenter'
+  # if you need to change rackspace_auth_url (e.g. if you need to use Rackspace London)
+  # config.rackspace_auth_url = "lon.auth.api.rackspacecloud.com"
+  config.fog_directory = 'mediacenter'
 
-#   # Invalidate a file on a cdn after uploading files
-#   # config.cdn_distribution_id = "12345"
-#   # config.invalidate = ['file1.js']
+  # Invalidate a file on a cdn after uploading files
+  # config.cdn_distribution_id = "12345"
+  # config.invalidate = ['file1.js']
 
-#   # Increase upload performance by configuring your region
-#   # config.fog_region = 'eu-west-1'
-#   #
-#   # Don't delete files from the store
-#   # config.existing_remote_files = "keep"
-#   #
-#   # Automatically replace files with their equivalent gzip compressed version
-#   # config.gzip_compression = true
-#   #
-#   # Use the Rails generated 'manifest.yml' file to produce the list of files to
-#   # upload instead of searching the assets directory.
-#   # config.manifest = true
-#   #
-#   # Fail silently.  Useful for environments such as Heroku
-#   # config.fail_silently = true
-# end
+  # Increase upload performance by configuring your region
+  # config.fog_region = 'eu-west-1'
+  #
+  # Don't delete files from the store
+  # config.existing_remote_files = "keep"
+  #
+  # Automatically replace files with their equivalent gzip compressed version
+  # config.gzip_compression = true
+  #
+  # Use the Rails generated 'manifest.yml' file to produce the list of files to
+  # upload instead of searching the assets directory.
+  # config.manifest = true
+  #
+  # Fail silently.  Useful for environments such as Heroku
+  # config.fail_silently = true
+end
