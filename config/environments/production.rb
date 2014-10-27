b915869c3c13f3ed4a091229b5e9b6f61acf6da9
e <e>
2014-10-27 16:01:05 -0600

    config assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ end
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -61,7 +61,7 @@ end
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
-  config.assets.prefix = "/production/assets"
+  #config.assets.prefix = "/production/assets"
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
